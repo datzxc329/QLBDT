cabc00f87e40ea08f541a84cf2daa624bdbc26c0
datzxc329 <phamtiendat1900@gmail.com>
2023-06-25 23:44:24 +0700

    init project

---
+++ modules/content/giothieu.php
@@ -0,0 +1,28 @@
+<div class="col-xs-12 col-sm-8 col-md-12 ">
+    <div class="name_cart">
+        <h4 class="title"><span>GIỚI THIỆU WEBSITE</span></h4>
+      </div>
+      <p>
+        Với tốc độ phát triển như vũ bảo của công nghệ. Các ứng dụng ngày càng nhiều và đòi hỏi cấu hình điện thoại trở lên mạnh hơn để đáp ứng với những nhu cầu đòi đặt ra. Các sản phẩm điện thoại thông minh luôn được nhận sự quan tâm từ phá đông đảo người dùng. Để tìm được địa điểm bán điện thoại uy tín cũng là vấn đề gây âu lo nhất từ phía người dùng.
+
+Cam kết của Thế Giới Di Động
+Thế giới di động cam kết đặt KHÁCH HÀNG LÀM TRUNG TÂM trong mọi suy nghĩ và hành động của mình.
+
+Thế giới di động cam kết mang đến cho nhân viên một môi trường làm việc TÔN TRỌNG và CÔNG BẰNG.
+
+Thế giới di động cam kết mang đến cho quản lý: Một SÂN CHƠI công bằng để thi thố tài năng / Một cam kết cho một cuộc sống cá nhân SUNG TÚC / Một vị trí xã hội được người khác KÍNH NỂ.
+
+Thế giới di động cam kết mang đến cho các đối tác sự TÔN TRỌNG.
+
+Thế giới di động cam kết mang đến cho nhà đầu tư giá trị doanh nghiệp GIA TĂNG KHÔNG NGỪNG.
+
+Thế giới di động cam kết đóng góp cho cộng đồng thông qua việc tạo nhiều ngàn việc làm và đóng góp đầy đủ thuế cho ngân sách nhà nước
+
+Giá trị cốt lõi của nhân viên
+Tận tâm với Khách hàng
+Trung thực
+Nhận trách nhiệm
+Yêu thương và hỗ trợ đồng đội
+Máu lửa trong công việc
+      </p>
+</div>
